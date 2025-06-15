7158331b76c609e799ee91099baa33584f36cc2e
Corie Watson <watson.corie@gmail.com>
2025-06-15 09:13:06 +0100

    improved code dynamism, validation, and made location param entry simpler with a select

--- functions/src/validation/query-validation.ts
+++ functions/src/validation/query-validation.ts
@@ -1,35 +1,23 @@
-import { Query, QueryType, TargetType } from "../types/Query";
+import { ATTRIBUTE_KEY, ID_KEY, Query, QueryType, TARGET_KEY, TargetType, TYPE_KEY, VALUE_KEY } from "../types/Query";
 import { warn } from 'firebase-functions/logger';
 
 export function validateQuery(query: Query): void {
-  if (!query) {
-    throw new Error("Query is missing");
+  if (typeof query !== 'object') {
+    throw new Error("Query must be provided as an object (or of type 'map')");
   }
 
   if (Object.keys(query).length === 0) {
     throw new Error("Query is empty");
   }
 
-  if (typeof query !== 'object') {
-    throw new Error("Query must be an object (or of type 'map')");
-  }
-
-  if (!query.id) {
-    throw new Error("Query ID ('id') is missing");
-  }
-
-  if (typeof query.id !== 'string') {
-    throw new Error("Query ID ('id') must be a string");
+  if (typeof query[ID_KEY] !== 'string') {
+    throw new Error(`Query ID ('${ID_KEY}') must be provided as a string`);
   }
 
   validateQueryType(query);
 
-  if (!query.value) {
-    throw new Error("Query value ('value') is missing");
-  }
-
-  if (typeof query.value !== 'string') {
-    throw new Error("Query value ('value') must be a string");
+  if (typeof query[VALUE_KEY] !== 'string') {
+    throw new Error(`Query value ('${VALUE_KEY}') must be provided as a string`);
   }
 
   validateTargetType(query);
@@ -37,53 +25,47 @@ export function validateQuery(query: Query): void {
 }
 
 function validateQueryType(query: Query): void {
-  if (!query.type) {
-    throw new Error("Query type ('type') is missing");
+  if (typeof query[TYPE_KEY] !== 'string') {
+    throw new Error(`Query type ('${TYPE_KEY}') must be provided as a string`);
   }
 
-  if (typeof query.type !== 'string') {
-    throw new Error("Query type ('type') must be a string");
+  if (!Object.values(QueryType).includes(query[TYPE_KEY])) {
+    throw new Error(`Invalid query type ('${TYPE_KEY}'): '${query[TYPE_KEY]}'. Valid types are: ${Object.values(QueryType).join(', ')}`);
   }
 
-  if (!Object.values(QueryType).includes(query.type)) {
-    throw new Error(`Invalid query type ('type'): '${query.type}'. Valid types are: ${Object.values(QueryType).join(', ')}`);
-  }
-
-  if (query.type === QueryType.XPATH) {
-    throw new Error("Query type ('type') cannot be 'xpath'. This is not supported currently.");
+  // TODO: Remove when supported
+  if (query[TYPE_KEY] === QueryType.XPATH) {
+    throw new Error(`Query type ('${TYPE_KEY}') cannot be 'xpath'. This is not supported currently.`);
   }
 }
 
 function validateTargetType(query: Query): void {
-  if (!query.target) {
-    throw new Error("Target type ('target') is missing");
+  if (typeof query[TARGET_KEY] !== 'string') {
+    throw new Error(`Target type ('${TARGET_KEY}') must be provided as a string`);
   }
 
-  if (typeof query.target !== 'string') {
-    throw new Error("Target type ('target') must be a string");
-  }
-
-  if (!Object.values(TargetType).includes(query.target)) {
-    throw new Error(`Invalid target type ('target'): '${query.target}'. Valid types are: ${Object.values(TargetType).join(', ')}`);
+  if (!Object.values(TargetType).includes(query[TARGET_KEY])) {
+    throw new Error(`Invalid target type ('${TARGET_KEY}'): '${query[TARGET_KEY]}'. Valid types are: ${Object.values(TargetType).join(', ')}`);
   }
 }
 
 function validateAttributeExtraction(query: Query): void {
-  if (!query.attr) {
+  // No attribute name provided, but that's fine, as target not specified as targetting attribute
+  if (!query[ATTRIBUTE_KEY] && query[TARGET_KEY] !== TargetType.ATTRIBUTE) {
     return;
   }
 
   // If the target is not attribute and an attr is defined, warn the user
-  if (query.target !== TargetType.ATTRIBUTE) {
-    warn(`${query.id} defines a '${query.target}' target, which does not support 'attr' extraction.`);
+  if (query[TARGET_KEY] !== TargetType.ATTRIBUTE) {
+    warn(`${query[ID_KEY]} defines a '${TARGET_KEY}' target, which does not support '${ATTRIBUTE_KEY}' extraction.`);
   }
 
-  if (typeof query.attr !== 'string') {
-    throw new Error("Attribute name ('attr') must be a string");
+  if (typeof query[ATTRIBUTE_KEY] !== 'string') {
+    throw new Error(`Attribute name ('${ATTRIBUTE_KEY}') must be provided as a string`);
   }
 
   // If target is attribute but no attr is provided, throw an error
-  if (query.target === TargetType.ATTRIBUTE && !query.attr) {
-    throw new Error("Attribute name ('attr') is required when target type is 'attribute'");
+  if (query.target === TargetType.ATTRIBUTE && !query[ATTRIBUTE_KEY]) {
+    throw new Error(`Attribute name ('${ATTRIBUTE_KEY}') is required when target type is 'attribute'`);
   }
 }
