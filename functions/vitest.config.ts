8cc85ce9a743ea7007fabd5642a0881cc1953958
Corie Watson <watson.corie@gmail.com>
2025-07-25 23:25:04 +0100

    feat: add codecov

--- functions/vitest.config.ts
+++ functions/vitest.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
     include: ['**/*.test.ts'],
     exclude: ['node_modules/**', 'lib/**'],
     setupFiles: ['./src/test/setup.ts'],
+    coverage: {
+      include: ['src/**'],
+      exclude: ['lib/**', 'node_modules/**', '**/*.test.ts', '**/test/**'],
+      reporter: ['text', 'json', 'html', 'clover']
+    }
   },
   resolve: {
     alias: {
